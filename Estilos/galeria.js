7aef094d453d4ae561baaadff98849555c23b946
Betaniadwell <omyness@yahoo.es>
2024-05-29 17:26:14 -0300

    Finalizado

---
+++ Estilos/galeria.js
@@ -0,0 +1,28 @@
+var myImage=document.querySelector("#imageid");
+
+function FirstImage()
+{
+  myImage.src="Infantiles.jpg";
+}
+
+function SecondImage()
+{
+  myImage.src="BillyGraham.jpg";
+}
+
+function ThridImage()
+{
+  myImage.src="EricFrom.jpg";
+}
+
+function QuarterImage()
+{
+  myImage.src="Freud.jpg";
+}
+
+function FifthImage()
+{
+  myImage.src="Terminator.jpg";
+}
+
+
