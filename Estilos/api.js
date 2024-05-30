345d80451a214a6f8da7675c861dc77440b5bde7
Betaniadwell <omyness@yahoo.es>
2024-05-30 16:03:49 -0300

    Colors Dom

--- Estilos/api.js
+++ Estilos/api.js
@@ -7,3 +7,27 @@ fetch(URL)
     const img = document.querySelector("img");
     img.src = data[0].url; });
 
+//////////////////////////////////////////////
+
+/* Change color*/
+const boton = document.querySelector('button');
+const color = document.getElementById('color');
+
+function generar(){
+let digitos = '0123456789ABCDEF';
+let colorHex = "#";
+
+for (let i=0; i < 6; i++) {
+ let indiceAl= Math.floor(Math.random() * 16) ;
+ colorHex += digitos[indiceAl];
+}
+ return colorHex;
+
+}
+
+boton.addEventListener('click', function() {
+  let colorAl = generar();
+  color.textContent = colorAl;
+  document.body.style.backgroundColor= colorAl;
+});
+
